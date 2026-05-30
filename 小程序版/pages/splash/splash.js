a4165cec9244c74cfdaedfa9c4e76a2751aeba2d
1426599349 <1426599349@qq.com>
2026-05-30 12:18:59 +0800

    新增：动态启动封面（深色背景+呼吸动画，上划进入首页，仅首次展示）

---
+++ 小程序版/pages/splash/splash.js
@@ -0,0 +1,32 @@
+Page({
+  data: { fadeOut: false },
+  _startY: 0,
+
+  onLoad() {
+    // 如果已经看过封面，直接进入
+    if (wx.getStorageSync('splashShown')) {
+      this.enterApp();
+    }
+  },
+
+  onTouchStart(e) {
+    this._startY = e.touches[0].clientY;
+  },
+
+  onTouchEnd(e) {
+    const dy = this._startY - e.changedTouches[0].clientY;
+    if (dy > 60) {
+      this.enterApp();
+    }
+  },
+
+  enterApp() {
+    if (this._entered) return;
+    this._entered = true;
+    this.setData({ fadeOut: true });
+    wx.setStorageSync('splashShown', true);
+    setTimeout(() => {
+      wx.switchTab({ url: '/pages/chat/chat' });
+    }, 350);
+  },
+});
